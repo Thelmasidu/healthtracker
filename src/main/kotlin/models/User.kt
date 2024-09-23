eb161fca09ebb124384a98da344e9fa1247ceace
Thelmasidu <20109631@mail.wit.ie>
2024-09-23 22:17:53 +0100

    Update User.kt
    Updated the user class

--- src/main/kotlin/models/User.kt
+++ src/main/kotlin/models/User.kt
@@ -3,5 +3,8 @@ package models
 data class User (
     var id: Int = -1,
     var name:String = "no name yet",
-    var email:String = "no email yet"
-)
+    var email:String = "no email yet",
+    var weight:Double = 12.0,
+    var height:Float = 3.123f,
+    var gender:String = ""
+    )
