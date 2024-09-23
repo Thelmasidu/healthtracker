17170c571bf203fc5b87b6a95776f595a0a998f2
Thelmasidu <20109631@mail.wit.ie>
2024-09-23 22:16:10 +0100

    Update build.gradle.kts
    Update for plugins and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.9.20"
 }
+
 
 group = "ie.setu"
 version = "1.0-SNAPSHOT"
@@ -11,6 +14,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
+
 }
 
 tasks.test {
@@ -20,3 +25,4 @@ tasks.test {
 kotlin {
     jvmToolchain(16)
 }
+
