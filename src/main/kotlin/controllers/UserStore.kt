e7e672202314e83d806a0565ef14b085a0bdc4c8
Thelmasidu <20109631@mail.wit.ie>
2024-09-30 20:04:49 +0100

    Refactored UserStore class to improve user deletion functionality and added an update method
    Removed deprecated delete(User?) method: The commented-out method delete(user: User?) has been fully removed to avoid redundancy and confusion.
    Introduced delete(userId: Int) method: Refactored to allow user deletion based on the user's id. This method first searches for the user and deletes them if found, returning a Boolean indicating success.
    Added update(user: User) method: This method allows updating user details such as name, email, gender, height, and weight based on the user's id. It returns true if the update is successful, and false if the user is not found.

--- src/main/kotlin/controllers/UserStore.kt
+++ src/main/kotlin/controllers/UserStore.kt
@@ -22,10 +22,34 @@ class UserStore {
         return users.find { p -> p.id == id }
     }
 
-    fun delete(user: User?): Boolean {
-        return users.remove(user)
+//    fun delete(user: User?): Boolean {
+//        return users.remove(user)
+//    }
+
+    fun delete(userId: Int): Boolean {
+        val user = findOne(userId)
+        return if (user != null) {
+            users.remove(user)
+            true
+        } else {
+            false
+        }
     }
 
+
+
+    fun update(user: User): Boolean {
+        val foundUser = findOne(user.id)
+        if (foundUser != null) {
+            foundUser.name = user.name
+            foundUser.email = user.email
+            foundUser.gender = user.gender
+            foundUser.height = user.height
+            foundUser.weight = user.weight
+            return true
+        }
+        return false
+    }
 
 
 }
